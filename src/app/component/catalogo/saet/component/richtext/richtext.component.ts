a5eecaa23df576db2f4f61e163c9e519e57fb7e5
byron <byronp@creaelsalvador.com>
2024-04-23 20:33:23 -0600

    feat: creacion de richtext refs: #

---
+++ src/app/component/catalogo/saet/component/richtext/richtext.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'app-richtext',
+  templateUrl: './richtext.component.html',
+  styleUrls: ['./richtext.component.css']
+})
+export class RichtextComponent implements OnChanges{
+  @Input() text: string = "xxx";
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes["text"]) {
+      console.log('Text has changed:', changes["text"].currentValue);
+    }
+  }
+}
