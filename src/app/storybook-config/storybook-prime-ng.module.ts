c12aec6e28dcd207155b9ffde99075f9cc335f1d
byron <byronp@creaelsalvador.com>
2024-05-09 14:32:11 -0600

    feat: configuraciones storybook refs: #

--- src/app/storybook-config/storybook-prime-ng.module.ts
+++ src/app/storybook-config/storybook-prime-ng.module.ts
@@ -3,17 +3,22 @@ import { CommonModule } from '@angular/common';
 import {EditorModule} from "primeng/editor";
 import {FormsModule} from "@angular/forms";
 import {FieldsetModule} from "primeng/fieldset";
+import {ButtonModule} from "primeng/button";
+import {TableModule} from "primeng/table";
 
 @NgModule({
   imports: [
     CommonModule,
     EditorModule,
-    FieldsetModule
+    FieldsetModule,
+    TableModule
   ],
   exports: [
     EditorModule,
     FormsModule,
     FieldsetModule,
+    ButtonModule,
+    TableModule
   ],
   providers: []
 })
