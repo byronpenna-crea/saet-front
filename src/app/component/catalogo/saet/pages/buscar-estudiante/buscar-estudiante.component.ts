fe5a62628fc617b203c038f6ed768aef0dcc456a
byron <byronp@creaelsalvador.com>
2024-05-15 12:00:49 -0600

    feat: mejora en pantalla de busqueda refs: #

--- src/app/component/catalogo/saet/pages/buscar-estudiante/buscar-estudiante.component.ts
+++ src/app/component/catalogo/saet/pages/buscar-estudiante/buscar-estudiante.component.ts
@@ -1,7 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, TemplateRef, ViewChild} from '@angular/core';
 import {ButtonStyle, SaetButtonArgs} from "../../component/saet-button/saet-button.component";
 import {IconComponent} from "../../shared/component.config";
 import {TableColumn} from "../../component/saet-table/saet-table.component";
+import {Route, Router} from "@angular/router";
+
 interface Estudiante {
   nie: string;
   primerNombre: string;
@@ -9,6 +11,7 @@ interface Estudiante {
   fechaNacimiento: string;
   EstadoApoyo: string;
   verDetalle: string;
+  Acciones: null;
 }
 @Component({
   selector: 'app-buscar-estudiante',
@@ -16,16 +19,25 @@ interface Estudiante {
   styleUrls: ['./buscar-estudiante.component.css']
 })
 export class BuscarEstudianteComponent {
+  inputNIE: string = '';
+  onInputChange(value: string) {
+    this.inputNIE = value;
+  }
+  constructor(private router: Router) {}
+
   showTable = false;
+
   studentTableColumns:TableColumn<Estudiante>[] = [
     { key: "nie", header: "NIE" },
     { key: "primerApellido", header: "Primer apellido" },
     { key: "primerNombre", header: "Primer nombre"},
     { key: "fechaNacimiento", header: "Fecha nacimiento" },
-    { key: "EstadoApoyo", header: "Estado de apoyo" }
+    { key: "EstadoApoyo", header: "Estado de apoyo" },
+    { key: "Acciones", header: "Acciones" }
   ];
+
   studentData: Estudiante[] = [
-    {nie: "1234", EstadoApoyo: "Indefinido", fechaNacimiento: "23/12/2010", primerNombre: "Carlos", primerApellido: "Mejia", verDetalle: "Ver"}
+    {nie: "1234", EstadoApoyo: "Indefinido", fechaNacimiento: "23/12/2010", primerNombre: "Carlos", primerApellido: "Mejia", verDetalle: "Ver", Acciones: null}
   ]
   toggleTable() {
     this.showTable = !this.showTable;
@@ -36,6 +48,15 @@ export class BuscarEstudianteComponent {
     text: "Buscar"
   }
 
+  cleanInput() {
+    console.log('clean input');
+    this.inputNIE = ''
+  }
 
+  navigateToDetails() {
+    if (this.inputNIE) {
+      this.router.navigate(['/menu/saet-datos-estudiante',  this.inputNIE ]);
+    }
+  }
 
 }
