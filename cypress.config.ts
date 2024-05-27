e69ee7d6b604076526c6913e1edba2aa0ec42982
byron <byronp@creaelsalvador.com>
2024-05-27 14:05:18 -0600

    feat: cypress refs: #

---
+++ cypress.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "cypress";
+import * as fs from "fs";
+
+export default defineConfig({
+  e2e: {
+    chromeWebSecurity: false,
+    baseUrl: 'http://192.168.1.12',
+    video: true,
+    videosFolder: "cypress/videos",
+    setupNodeEvents(on, config) {
+      config.env = {
+        ...config.env,
+        "CYPRESS_SSL_VERIFY": false
+      }
+
+      return config
+    },
+
+    supportFile: 'cypress/support/index.ts',
+  },
+});
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.feature$/,
+        use: [
+          {
+            loader: '@badeball/cypress-cucumber-preprocessor/webpack',
+          },
+        ],
+      },
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+}
