e044b92c8006ff2d611dc5a1473a5dbf6f7de9c9
byron <byronp@creaelsalvador.com>
2024-06-07 13:46:10 -0600

    feat: to deploym refs: #

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     API_SERVER_URL: 'https://www.clases.edu.sv/api/',
-    API_SERVER_PORT: '443'
+    API_SERVER_PORT: '443',
+    API_SERVER_URL_SEGURIDAD: `https://www.clases.edu.sv/api/`
   };
-  
