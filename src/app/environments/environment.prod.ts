76978b39c570c507e3eb574deb46710218ff4063
byron <byronp@creaelsalvador.com>
2024-06-09 14:32:30 -0600

    feat: corregidas las url a nuevo servidor https refs: #

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_SERVER_URL: 'crea.ritsasv.com/sgpa_saet-0.0.1-SNAPSHOT/',
+    API_SERVER_URL: 'https://crea.ritsasv.com/sgpa_saet-0.0.1-SNAPSHOT/',
     API_SERVER_PORT: '443',
-    API_SERVER_URL_SEGURIDAD: `crea.ritsasv.com/sgpa_saet-0.0.1-SNAPSHOT/`
+    API_SERVER_URL_SEGURIDAD: `https://crea.ritsasv.com/sgpa_saet-0.0.1-SNAPSHOT/`
   };
