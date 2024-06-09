c15f5617fdcbd32e93d56af3800d73f8b249d8c2
byron <byronp@creaelsalvador.com>
2024-06-09 10:33:18 -0600

    feat: cambiado servidor target en prod refs: #

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_SERVER_URL: 'https://www.clases.edu.sv/api/',
+    API_SERVER_URL: 'http://ec2-3-90-163-195.compute-1.amazonaws.com/sgpa_saet-0.0.1-SNAPSHOT/',
     API_SERVER_PORT: '443',
     API_SERVER_URL_SEGURIDAD: `http://ec2-3-90-163-195.compute-1.amazonaws.com/sgpa_saet-0.0.1-SNAPSHOT/`
   };
