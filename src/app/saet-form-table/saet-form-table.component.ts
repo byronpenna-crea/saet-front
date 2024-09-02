4a88de84c2d347559084ad0628c272df5177480f
Byron Peña <byronpenna@users.noreply.github.com>
2024-09-02 18:09:09 -0400

    feat: form table para familiares

---
+++ src/app/saet-form-table/saet-form-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saet-form-table',
+  templateUrl: './saet-form-table.component.html',
+  styleUrls: ['./saet-form-table.component.css']
+})
+export class SaetFormTableComponent {
+
+}
