11ed0796d8a15a7e204c8ad225db162af82806a9
Byron Peña <byronpenna@users.noreply.github.com>
2024-09-09 23:10:26 -0400

    feat: historia tabs buttons

---
+++ src/app/component/catalogo/saet/component/saet-tab-buttons/__stories__/saet-tab-buttons.stories.ts
@@ -0,0 +1,42 @@
+import {Meta, moduleMetadata, Story} from "@storybook/angular";
+import {StorybookPrimeNgModule} from "../../../../../../storybook-config/storybook-prime-ng.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {SaetTabButtonsComponent} from "../saet-tab-buttons.component";
+
+export default {
+  title: 'saet-tab-buttons',
+  component: SaetTabButtonsComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [
+        StorybookPrimeNgModule,
+        BrowserAnimationsModule
+      ],
+    }),
+  ]
+  /*,
+  argTypes: {
+
+  }*/
+} as Meta;
+
+const Template: Story<SaetTabButtonsComponent> = (args: SaetTabButtonsComponent) => ({
+  component: SaetTabButtonsComponent,
+  props: {
+    ...args
+  },
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  tabs: [
+    {
+      text: 'Datos generales',
+      href: '#',
+      readOnly: false,
+      testId: '',
+      selected: false,
+      onClick: () => {console.log('Datos generales click')}
+    }
+  ]
+}
--- src/app/component/catalogo/saet/component/saet-tab-buttons/saet-tab-buttons.component.ts
+++ src/app/component/catalogo/saet/component/saet-tab-buttons/saet-tab-buttons.component.ts
@@ -1,11 +1,30 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
 
+export interface ITabButton {
+  text: string;
+  href?: string;
+  readOnly: boolean;
+  testId?: string;
+  selected: boolean;
+  onClick?: () => void;
+}
 @Component({
   selector: 'app-saet-tab-buttons',
   templateUrl: './saet-tab-buttons.component.html',
   styleUrls: ['./saet-tab-buttons.component.css'],
-  encapsulation: ViewEncapsulation.Emulated,
+  encapsulation: ViewEncapsulation.None,
 })
 export class SaetTabButtonsComponent {
   @Input() readOnly: boolean = false;
+  @Input() tabs:ITabButton[] = [];
+
+  handleClick(event: Event, tab: ITabButton) {
+    if (tab.readOnly || tab.onClick) {
+      event.preventDefault();
+    }
+
+    if (tab.onClick) {
+      tab.onClick();
+    }
+  }
 }
