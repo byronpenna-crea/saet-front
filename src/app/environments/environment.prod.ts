136824a95578e5dfcc8be9b4ef76250a6d55f247
Byron Peña <byronpenna@users.noreply.github.com>
2024-10-11 22:11:43 -0400

    fix login endpoint

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     API_SERVER_URL: '192.168.1.12',
     API_SERVER_PORT: '443',
-    API_SERVER_URL_SEGURIDAD: `https://crea.ritsasv.com/sgpa_saet-0.0.1-SNAPSHOT/`
+    API_SERVER_URL_SEGURIDAD: `https://crea.ritsasv.com/sgpa_saet-0.0.1-SNAPSHOT`
   };
 
 
