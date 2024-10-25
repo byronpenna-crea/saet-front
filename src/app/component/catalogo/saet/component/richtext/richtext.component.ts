dbff3de3ea313aa33da6a6177912654a3e4f1232
Byron Peña <byronpenna@users.noreply.github.com>
2024-10-25 00:38:59 -0400

    feat: evitado que pegue imagenes

--- src/app/component/catalogo/saet/component/richtext/richtext.component.ts
+++ src/app/component/catalogo/saet/component/richtext/richtext.component.ts
@@ -26,9 +26,48 @@ export class RichtextComponent {
   onInputChange(event: any, name: string) {
     const newValue = event.htmlValue;
     this.text = newValue;
+    this.filterImagesFromURLs();
     this.emitInputChange(newValue, name);
+
   }
 
+  setupEditor() {
+    this.preventImagePaste();
+  }
+
+  // Bloquear la inserción de imágenes desde URLs
+  preventImagePaste() {
+    const editorElement = document.querySelector('.ql-editor');
+    if (editorElement) {
+      editorElement.addEventListener('paste', (event: any) => {
+        const clipboardData = event.clipboardData;
+        if (clipboardData) {
+          const pastedData = clipboardData.getData('text/html');
+          if (pastedData && this.containsImageFromURL(pastedData)) {
+            event.preventDefault();
+          }
+        }
+      });
+    }
+  }
+
+  containsImageFromURL(content: string): boolean {
+    const regex = /<img[^>]+src=["'](http|https):\/\/[^"']*["']/;
+    return regex.test(content);
+  }
+
+  filterImagesFromURLs() {
+    const editorElement = document.querySelector('.ql-editor');
+    if (editorElement) {
+      const images = editorElement.querySelectorAll('img');
+      images.forEach((img) => {
+        const src = img.getAttribute('src');
+        if (src && (src.startsWith('http') || src.startsWith('https'))) {
+          img.remove(); // Eliminar la imagen del contenido
+        }
+      });
+    }
+  }
   private emitInputChange(newValue: string, key: string) {
     this.inputChange.emit({
       key: key,
